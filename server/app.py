84a8306f784f595b7af889684c615e105085af21
Janemwaura <mwaura461@gmail.com>
2024-03-03 18:28:24 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -19,16 +19,46 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.get(id)
+    if animal:
+        response_body = f'<ul><li>Name: {animal.name}</li>'
+        response_body += f'<li>Species: {animal.species}</li>'
+        if animal.zookeeper:
+            response_body += f'<li>Zookeeper: {animal.zookeeper.name}</li>'
+        if animal.enclosure:
+            response_body += f'<li>Enclosure: {animal.enclosure.environment}</li>'
+        response_body += '</ul>'
+        return make_response(response_body, 200)
+    else:
+        return make_response('<h1>Animal not found</h1>', 404)
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.get(id)
+    if zookeeper:
+        response_body = f'<ul><li>Name: {zookeeper.name}</li>'
+        response_body += f'<li>Birthday: {zookeeper.birthday}</li>'
+        response_body += '<li>Animals:</li><ul>'
+        for animal in zookeeper.animals:
+            response_body += f'<li>{animal.name} ({animal.species})</li>'
+        response_body += '</ul></ul>'
+        return make_response(response_body, 200)
+    else:
+        return make_response('<h1>Zookeeper not found</h1>', 404)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
-
+    enclosure = Enclosure.query.get(id)
+    if enclosure:
+        response_body = f'<ul><li>Environment: {enclosure.environment}</li>'
+        response_body += f'<li>Open to Visitors: {enclosure.open_to_visitors}</li>'
+        response_body += '<li>Animals:</li><ul>'
+        for animal in enclosure.animals:
+            response_body += f'<li>{animal.name} ({animal.species})</li>'
+        response_body += '</ul></ul>'
+        return make_response(response_body, 200)
+    else:
+        return make_response('<h1>Enclosure not found</h1>', 404)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
